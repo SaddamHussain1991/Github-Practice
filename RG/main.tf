dabf2ccaf9702618123196fddf9ee9fd578543ad
SaddamHussain1991 <161189239+SaddamHussain1991@users.noreply.github.com>
2025-10-04 14:51:02 +0530

    Update main.tf

--- RG/main.tf
+++ RG/main.tf
@@ -1,22 +1,23 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg1" {
   name     = "rg-github"
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg2" {
 
   name     = "rg-github102"
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg3" {
 
   name     = "rg-github100"
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "rg4" {
 
   name     = "rg-github1000"
   location = "West Europe"
 }
+
