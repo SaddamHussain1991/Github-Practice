66cac826507c89882bb220df737f7854a4ad40b8
Saddam Hussain <saddamhussain1006@gmail.com>
2025-10-04 14:14:13 +0530

    added 102

--- RG/main.tf
+++ RG/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-github"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-github102"
+  location = "West Europe"
+}
